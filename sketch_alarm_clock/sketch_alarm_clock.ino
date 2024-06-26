4fb0e9b4b252272b1f81a462b120b45997fb2bb1
kirby-b <kirby.brodie66@gmail.com>
2024-06-26 18:45:06 -0400

    Update sketch_alarm_clock.ino

--- sketch_alarm_clock/sketch_alarm_clock.ino
+++ sketch_alarm_clock/sketch_alarm_clock.ino
@@ -1,1 +1,10 @@
-// Code will be here
+void setup() 
+{
+    // Initialize RTC pins, LCD screen pins, and Buzzer pins
+}
+
+void loop() {
+    // Checks time and sets off buzzer if it reads a certain time. 
+    // Will also display time on the screen
+    // It would be a good idea to also implement use of a remote for setting time
+}
