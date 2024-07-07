082e7b64e54bf44f800d7e96551204d236e699da
kirby-b <kirby.brodie66@gmail.com>
2024-07-07 18:12:38 -0400

    LCD prints date and time now
    I made a friggin clock
    
    Now lets make an alarm

--- sketch_alarm_clock/sketch_alarm_clock.ino
+++ sketch_alarm_clock/sketch_alarm_clock.ino
@@ -1,44 +1,91 @@
-void setup() 
-{
-    // Initialize RTC pins, LCD screen pins, and Buzzer pins
-}
-
-void loop() {
-    // Checks time and sets off buzzer if it reads a certain time. 
-    // Will also display time on the screen
-    // It would be a good idea to also implement use of a remote for setting time
-}
-
-
-// Code from the lcd example
-// // include the library code:
-// #include <LiquidCrystal.h>
-
-// // initialize the library with the numbers of the interface pins
-// LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
-// void setup() {
-//   // set up the LCD's number of columns and rows:
-//   lcd.begin(16, 2);
-//   // Print a message to the LCD.
-//   lcd.print("Hello, World!");
+// void setup() 
+// {
+//     // Initialize RTC pins, LCD screen pins, and Buzzer pins
 // }
 
 // void loop() {
-//   // set the cursor to column 0, line 1
-//   // (note: line 1 is the second row, since counting begins with 0):
-//   lcd.setCursor(0, 1);
-//   // print the number of seconds since reset:
-//   lcd.print(millis() / 1000);
+//     // Checks time and sets off buzzer if it reads a certain time. 
+//     // Will also display time on the screen
+//     // It would be a good idea to also implement use of a remote for setting time
 // }
 
+// include the library code:
+#include <LiquidCrystal.h>
 
-// Code from active buzzer example
-// int buzzer = 12;//the pin of the active buzzer
-// void setup()
-// {
-//   pinMode(buzzer, OUTPUT); //initialize the buzzer pin as an output
-// }
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+int buzzer = 12;//the pin of the active buzzer
+
+char daysOfTheWeek[7][12] = {
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+};
+
+void setup () {
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT); //initialize the buzzer pin as an output
+  // SETUP RTC MODULE
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1);
+  }
+
+  // automatically sets the RTC to the date & time on PC this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  lcd.begin(16, 2);
+  DateTime now = rtc.now();
+  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  lcd.print(' ');
+  lcd.print(now.day(), DEC);
+  lcd.print('/');
+  lcd.print(now.month(), DEC);
+  lcd.print('/');
+  lcd.print(now.year(), DEC);
+
+  // manually sets the RTC with an explicit date & time, for example to set
+  // January 21, 2021 at 3am you would call:
+  // rtc.adjust(DateTime(2021, 1, 21, 3, 0, 0));
+}
+
+void loop () {
+  DateTime now = rtc.now();
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(now.hour(), DEC);
+  lcd.print(':');
+  lcd.print(now.minute(), DEC);
+  lcd.print(':');
+  lcd.println(now.second(), DEC);
+
+  if (now.hour() == 0 && now.minute() == 0 && now.second() == 0){
+    lcd.setCursor(0, 0);
+    lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    lcd.print(' ');
+    lcd.print(now.day(), DEC);
+    lcd.print('/');
+    lcd.print(now.month(), DEC);
+    lcd.print('/');
+    lcd.print(now.year(), DEC);
+  }
+
+  delay(1000); // delay 1 seconds
+}
+
+
 // void loop()
 // {
 //   int sound_duration = 500;
